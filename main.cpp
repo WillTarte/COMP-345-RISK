059973fa9e68d1db55ad59a4f4929853a0dcbcdf
Thierry R. Le Bourdais <thierryr@orphic.ca>
2019-09-08 18:16:17 -0400

    Created the map.cpp utility file to read a txt file, extract vertex information and print the vertices. #4

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include "map.h"
+#include <cmath>
+#include <fstream>
+
 int main() 
 {
-    std::cout << "Hello, World!";
+    //read vertices from file
+    std::vector<std::vector <int>> map_vertices = getVertices("mapconfig.txt");
+    //display them
+    printVertices(map_vertices);
+
     return 0;
-    //test branch
 }
