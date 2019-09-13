a139409e699cc7460f61259b10992521217caffe
Unknown <claudia.lapalme@gmail.com>
2019-09-13 15:09:17 -0400

    #7 Implemented shuffled deck

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "include/Cards.h"
 int main() 
 {
-    std::cout << "Hello, World!";
+    Deck a = Deck(9);
+    a.createDeck();
+    int size = a.getNumberOfCards();
+    std::vector<CardType> *deckPointer = a.getDeck();
+
+    for (int i = 0; i < size; i++) {
+        std::cout << static_cast<int>(deckPointer->at(i)) << std::endl;
+    }
+
     return 0;
 }
