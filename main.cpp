603358e44c891ceac479c528a8f2b3395de9cd4b
Claudia Lapalme <32103530+ClaudiaLapalme@users.noreply.github.com>
2019-09-15 21:46:18 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
-#include "include/Cards.h"
 int main() 
 {
-    Deck a = Deck(9);
-    a.createDeck();
-    int size = a.getNumberOfCards();
-    std::vector<CardType> *deckPointer = a.getDeck();
-
-    for (int i = 0; i < size; i++) {
-        std::cout << static_cast<int>(deckPointer->at(i)) << std::endl;
-    }
+    std::cout << "Hello, World!";
 
     return 0;
 }
