13e41e64bff817fda16afcdb5b76ae4babd2d00c
phdumaresq <phdumaresq@gmail.com>
2019-09-17 20:28:28 -0400

    Added more headers to my header file

--- include/Dice.h
+++ include/Dice.h
@@ -3,24 +3,28 @@
 #ifndef COMP_345_PROJ_DICE_H
 #define COMP_345_PROJ_DICE_H
 
+class Dice {
+public:
+  Dice();
+  int roll();
+  std::vector<int> getHistory();
+
+private:
+  std::vector<int>* history;
+  void saveRoll();
+};
+
 class DiceRoller {
 public:
   DiceRoller(int numDice);
   std::vector<int> roll();
-  std::vector<int> getRollHistory();
+  std::vector<int> getHistory();
+  Dice[] getDice();
+  int getNumDice();
 
 private:
   int* numDice;
-  std::vector<Dice>* dice;
+  Dice[]* dice;
 };
-
-class Dice {
-public:
-  int roll();
-
-private:
-  std::vector<int>* pastRolls;
-  void saveRoll();
-}
 
 #endif // COMP_345_PROJ_DICE_H
