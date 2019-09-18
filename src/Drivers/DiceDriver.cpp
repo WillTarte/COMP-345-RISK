d075e091fa188e42bb791b5356a307d791203edd
phdumaresq <phdumaresq@gmail.com>
2019-09-17 20:34:10 -0400

    Added comments to tests and a range test

--- src/Drivers/DiceDriver.cpp
+++ src/Drivers/DiceDriver.cpp
@@ -2,10 +2,15 @@
 #include "../../include/Dice.h"
 
 int main() {
-  return test_requestMultipleDice() &&
-    test_returnCorrectNumOfDice();
+  return
+    test_requestMultipleDice() &&
+    test_returnCorrectNumOfDice() &&
+    test_valuesInCorrectRange();
 }
 
+/**
+ * Test to make sure that a user can create a dice roller with between 1 and 3 dice
+ */
 bool test_requestMultipleDice() {
   bool success = true;
 
@@ -23,6 +28,9 @@ bool test_requestMultipleDice() {
   return success;
 }
 
+/**
+ * Test to ensure that regardless of no failure, the dice are created.
+ */
 bool test_returnCorrectNumOfDice() {
   bool success = true;
 
@@ -36,14 +44,31 @@ bool test_returnCorrectNumOfDice() {
   return success;
 }
 
+/**
+ * Test to make sure all numbers resulting from rolls are between 1 and 6 (inclusive)
+ */
 bool test_valuesInCorrectRange() {
-  return false;
+  bool success = true;
+
+  Dice dice = Dice();
+  for (int i = 1; i < 100; i++) {
+    int result = dice.roll();
+    success = sucess && (result < 7 && result > 0);
+  }
+
+  return success;
 }
 
+/**
+ * Test to make sure the rolling of the dice is "fair"
+ */
 bool test_equalShareOfValues() {
   return false;
 }
 
+/**
+ * Test to ensure the history of rolls is properly remembered
+ */
 bool test_maintainPercentageOfRolls() {
   return false;
 }
