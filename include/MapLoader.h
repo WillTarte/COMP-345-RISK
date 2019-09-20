d9d06a0a89ea820ec8d2dfb298a562a45f9e150d
Thierry R. Le Bourdais <thierryr@orphic.ca>
2019-09-19 20:26:07 -0400

    refactored initmap() to improve readability.

--- include/MapLoader.h
+++ include/MapLoader.h
@@ -7,6 +7,8 @@
 #ifndef COMP_345_PROJ_MAPLOADER_H
 #define COMP_345_PROJ_MAPLOADER_H
 
+class string;
+
 class MapLoader{
 public:
     MapLoader(std::string mapFile);
@@ -15,7 +17,20 @@ public:
     Map* initMap();
 
 private:
-    static Map* initMapObject(std::string mn, std::vector<std::vector<std::string>> ctd, std::vector<std::vector<std::string>> cyd, std::vector<std::vector<int>> bd, bool vMap);
+    static Map* initMapObject(std::string* mapName, std::vector<std::vector<std::string>>* continentData, std::vector<std::vector<std::string>>* countryData, std::vector<std::vector<int>>* borderData, bool* vMap);
+    void splitLine(const std::string &line, std::vector<std::string> *pLineWords);
+    void getMapName(std::string *mapName, std::vector<std::string> *lineWords);
+
+    bool checkSection(std::string *mode, std::vector<std::string> *lineWords);
+
+    bool validateContinentLine(int *continentCount, std::vector<std::string> *lineWords, const int *lineCount, bool *validMap);
+
+    bool validateCountryLine(int *countryCount, std::vector<std::string> *lineWords, const int *lineCount, bool *validMap,
+                             int *countryID, const int *continentCount);
+
+    bool
+    validateBordersLine(std::vector<int> *lineNums, std::vector<std::string> *lineWords, const int *lineCount, bool *validMap,
+                        const int *countryCount);
 };
 
 #endif //COMP_345_PROJ_MAPLOADER_H
