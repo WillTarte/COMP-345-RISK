7422715e91e1b16ad7aab86ac76970d13a9ea0d4
phdumaresq <phdumaresq@gmail.com>
2019-10-01 12:23:53 -0400

    Implemented more tests in DiceDriver

--- src/Drivers/DiceDriver.cpp
+++ src/Drivers/DiceDriver.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include "../../include/Dice.h"
+#define assert(success) (success ? "\033[32mPass" : "\033[31mFail") << "\033[30m" << std::endl
 
 ////////////////////////////////////////////////////////////
 ///////////////////// Dice Tests ///////////////////////////
 ////////////////////////////////////////////////////////////
 bool test_Dice() {
-    return false;
+    try {
+        Dice *dice = new Dice();
+        delete(dice);
+        return true;
+    }
+    catch (int e) {
+        return false;
+
+    }
 }
 
 bool test_Dice_roll() {
@@ -21,7 +30,17 @@ bool test_Dice_roll() {
 }
 
 bool test_Dice_getHistory() {
-    return false;
+    bool success = true;
+
+    Dice* d = new Dice();
+    std::vector<u_int> history = {0, 0, 0, 0, 0, 0};
+    for (int i = 0; i < 100; i++) {
+        int outcome = d->roll();
+        history[outcome-1]++;
+        success = success && history[outcome-1] == d->getHistory()[outcome-1];
+    }
+
+    return success;
 }
 
 
@@ -33,9 +52,8 @@ bool test_DiceRoller() {
 
     for (u_int i = 1; i < 4; i++) {
         try {
-            DiceRoller roller = DiceRoller(i);
-            success = success && roller.getNumDice() == i;
-            success = success && roller.getDice().size() == i;
+            auto* roller = new DiceRoller(i);
+            success = success && roller->getDice().size() == i;
         }
         catch (int e) {
             success = false;
@@ -47,13 +65,14 @@ bool test_DiceRoller() {
 
 bool test_DiceRoller_roll() {
     bool success = true;
-    DiceRoller roller = DiceRoller(3);
-    auto outcome = roller.roll();
+    auto* roller = new DiceRoller(3);
+    std::vector<u_int> outcome = roller->roll();
 
     success = success && outcome.size() == 3;
-    success = success && outcome.at(0) > 0 && outcome.at(0) < 7;
-    success = success && outcome.at(1) > 0 && outcome.at(1) < 7;
-    success = success && outcome.at(2) > 0 && outcome.at(2) < 7;
+    success = success && outcome[0] > 0 && outcome[0] < 7;
+    success = success && outcome[1] > 0 && outcome[1] < 7;
+    success = success && outcome[2] > 0 && outcome[2] < 7;
+    success = success && outcome[0] <= outcome[1] && outcome[1] <= outcome[2];
 
     return success;
 }
@@ -73,44 +92,28 @@ bool test_DiceRoller_getNumDice() {
 ////////////////////////////////////////////////////////////
 ////////////////////// Run Tests ///////////////////////////
 ////////////////////////////////////////////////////////////
-void assert(bool success) {
-    if (success)
-        std::cout << "Pass" << std::endl;
-    else
-        std::cout << "Fail" << std::endl;
-}
-
 int main() {
+    std::cout << "\033[34m";
     std::cout << "--------------------------------------------------------" << std::endl;
     std::cout << "------------- Running tests for Dice class -------------" << std::endl;
     std::cout << "--------------------------------------------------------" << std::endl;
-    std::cout << "" << std::endl;
+    std::cout << "\033[30m";
 
-    std::cout << "----- Testing Dice constructor" << std::endl;
-    assert(test_Dice());
-    std::cout << "----- Testing Dice roll() method" << std::endl;
-    assert(test_Dice_roll());
-    std::cout << "----- Testing Dice getHistory()" << std::endl;
-    assert(test_Dice_getHistory());
+    std::cout << "Testing Dice constructor: " << assert(test_Dice());
+    std::cout << "Testing Dice roll() method: " << assert(test_Dice_roll());
+    std::cout << "Testing Dice getHistory() method: " << assert(test_Dice_getHistory());
 
-    std::cout << "" << std::endl;
+    std::cout << "\033[34m" << std::endl;
     std::cout << "--------------------------------------------------------" << std::endl;
     std::cout << "---------- Running tests for DiceRoller class ----------" << std::endl;
     std::cout << "--------------------------------------------------------" << std::endl;
-    std::cout << "" << std::endl;
+    std::cout << "\033[30m";
 
-    std::cout << "----- Testing DiceRoller constructor" << std::endl;
-    assert(test_DiceRoller());
-    std::cout << "----- Testing DiceRoller roll() method" << std::endl;
-    assert(test_DiceRoller_roll());
-    std::cout << "----- Testing DiceRoller getHistory() method" << std::endl;
-    assert(test_DiceRoller_getHistory());
-
-    std::cout << "----- Testing DiceRoller getDice() method" << std::endl;
-    assert(test_DiceRoller());
-
-    std::cout << "----- Testing DiceRoller getNumDice() method" << std::endl;
-    assert(test_DiceRoller());
+    std::cout << "Testing DiceRoller constructor: " << assert(test_DiceRoller());
+    std::cout << "Testing DiceRoller roll() method: " << assert(test_DiceRoller_roll());
+    std::cout << "Testing DiceRoller getHistory() method: " << assert(test_DiceRoller_getHistory());
+    std::cout << "Testing DiceRoller getDice() method: " << assert(test_DiceRoller_getDice());
+    std::cout << "Testing DiceRoller getNumDice() method: " << assert(test_DiceRoller_getNumDice());
 
     return 0;
 }
