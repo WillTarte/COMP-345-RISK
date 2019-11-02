8e0640fa7da5ad1423459a3ee22df972391c168a
phdumaresq <phdumaresq@gmail.com>
2019-11-02 12:02:46 -0400

    Wrote the class declaration for ReinforcementPhase

--- include/ReinforcementPhase.h
+++ include/ReinforcementPhase.h
@@ -1,8 +1,18 @@
-//
-// Created by phdum on 2019-11-02.
-//
-
 #ifndef COMP_345_PROJ_REINFORCEMENTPHASE_H
 #define COMP_345_PROJ_REINFORCEMENTPHASE_H
+
+class ReinforcementPhase {
+public:
+    ReinforcementPhase(int player);
+    int getNumberOfArmies();
+    void placeArmies();
+
+private:
+    int* numberOfArmies;
+    int* player;
+    int countriesOwned();
+    int continentControlValue();
+    int cardExchange();
+};
 
 #endif //COMP_345_PROJ_REINFORCEMENTPHASE_H
