0c1686c2969ef9c6ab881c9c713e99794921ac60
phdumaresq <phdumaresq@gmail.com>
2019-11-02 23:25:00 -0400

    ReinforcementPhase driver and beginning of class methods

--- src/ReinforcementPhase.cpp
+++ src/ReinforcementPhase.cpp
@@ -1,9 +1,37 @@
 #include "ReinforcementPhase.h"
 
-ReinforcementPhase::ReinforcementPhase(int player) {}
-int ReinforcementPhase::getNumberOfArmies() {}
+ReinforcementPhase::ReinforcementPhase(int player) :
+        player(new int(player)), numberOfArmies(0) {}
+
+int ReinforcementPhase::getNumberOfArmies() {
+    auto countries = countriesOwned();
+    numberOfArmies += countries < 3 ? 3 : countries / 3;
+
+    auto controlValue = continentControlValue();
+    numberOfArmies += controlValue;
+
+    auto exchange = cardExchange();
+    numberOfArmies += exchange;
+
+    return *numberOfArmies;
+}
+
 void ReinforcementPhase::placeArmies() {}
 
-int ReinforcementPhase::countriesOwned() {}
-int ReinforcementPhase::continentControlValue() {}
-int ReinforcementPhase::cardExchange() {}
+int ReinforcementPhase::countriesOwned() {
+    auto countries = 0;
+
+    return countries;
+}
+
+int ReinforcementPhase::continentControlValue() {
+    auto value = 0;
+
+    return value;
+}
+
+int ReinforcementPhase::cardExchange() {
+    auto exchange = 0;
+
+    return exchange;
+}
