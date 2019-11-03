ceedb0d37fde0ae344ee5c4909fb4c42eae0b3ff
phdumaresq <phdumaresq@gmail.com>
2019-11-03 18:55:32 -0500

    Added beginning of driver test file

--- src/Drivers/ReinforcementPhaseDriver.cpp
+++ src/Drivers/ReinforcementPhaseDriver.cpp
@@ -1,4 +1,41 @@
-//
-// Created by phdum on 2019-11-02.
-//
+#include <iostream>
+
+bool test_ReinforcementPhase() {
+    bool pass = true;
+
+    return pass;
+}
+
+bool test_getNumberOfArmies() {
+    bool pass = true;
+
+    return pass;
+}
+
+bool test_placeArmies() {
+    bool pass = true;
+
+    return pass;
+}
+
+////////////////////////////////////////////////////////////
+////////////////////// Run Tests ///////////////////////////
+////////////////////////////////////////////////////////////
+
+#define assert(c, m, s) "Testing " << c << " " << m << "() method: " \
+    << (s ? "\033[32mPass" : "\033[31mFail") << "\033[30m" << std::endl
+
+int main() {
+    std::cout << "\033[34m";
+    std::cout << "--------------------------------------------------------" << std::endl;
+    std::cout << "------ Running tests for ReinforcementPhase class ------" << std::endl;
+    std::cout << "--------------------------------------------------------" << std::endl;
+    std::cout << "\033[30m";
+
+    std::cout << assert("ReinforcementPhase", "constructor", test_ReinforcementPhase());
+    std::cout << assert("ReinforcementPhase", "getNumberOfArmies", test_getNumberOfArmies());
+    std::cout << assert("ReinforcementPhase", "placeArmies", test_placeArmies());
+
+    return 0;
+}
 
