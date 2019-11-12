ab2abcd94dd6469e016c2727fd38a793c12dc1a4
phdumaresq <phdumaresq@gmail.com>
2019-11-12 16:19:01 -0500

    Added method definitions and delegation to each method given the context

--- src/PlayerStrategy.cpp
+++ src/PlayerStrategy.cpp
@@ -46,6 +46,34 @@ int AggressiveBotStrategy::intInput(StrategyContext context) {
 
     switch ((int) context) {
         case StrategyContext::ATTACK_FROM_COUNTRY:
+            count = attackFromCountryIndex();
+            break;
+        case StrategyContext::ATTACK_TO_COUNTRY:
+            count = attackToCountryIndex();
+            break;
+        case StrategyContext::ATTACK_DICE_COUNT:
+            attackNumDice();
+            break;
+        case StrategyContext::ATTACK_NEW_ARMY_COUNT:
+            attackNewArmies();
+            break;
+        case StrategyContext::DEFEND_DICE_COUNT:
+            defendNumDice();
+            break;
+        case StrategyContext::FORTIFY_FROM_COUNTRY:
+            fortifyFromCountryIndex();
+            break;
+        case StrategyContext::FORTIFY_TO_COUNTRY:
+            fortifyToCountryIndex();
+            break;
+        case StrategyContext::FORTIFY_ARMY_COUNT:
+            fortifyArmyCount();
+            break;
+        case StrategyContext::REINFORCE_ARMY_COUNT:
+            numArmies();
+            break;
+        case StrategyContext::REINFORCE_CARD_COUNT:
+            place();
             break;
         default: {
         }
@@ -53,6 +81,26 @@ int AggressiveBotStrategy::intInput(StrategyContext context) {
 
     return count;
 }
+
+int AggressiveBotStrategy::attackFromCountryIndex() {};
+
+int AggressiveBotStrategy::attackToCountryIndex() {};
+
+int AggressiveBotStrategy::attackNewArmies() {};
+
+int AggressiveBotStrategy::attackNumDice() {};
+
+int AggressiveBotStrategy::defendNumDice() {};
+
+int AggressiveBotStrategy::fortifyFromCountryIndex() {};
+
+int AggressiveBotStrategy::fortifyToCountryIndex() {};
+
+int AggressiveBotStrategy::fortifyArmyCount() {};
+
+int AggressiveBotStrategy::numArmies() {};
+
+int AggressiveBotStrategy::place() {};
 
 char BenevolentBotStrategy::yesOrNo(StrategyContext context) {
     char botChoice = 0;
@@ -78,6 +126,34 @@ int BenevolentBotStrategy::intInput(StrategyContext context) {
 
     switch ((int) context) {
         case StrategyContext::ATTACK_FROM_COUNTRY:
+            count = attackFromCountryIndex();
+            break;
+        case StrategyContext::ATTACK_TO_COUNTRY:
+            count = attackToCountryIndex();
+            break;
+        case StrategyContext::ATTACK_DICE_COUNT:
+            attackNumDice();
+            break;
+        case StrategyContext::ATTACK_NEW_ARMY_COUNT:
+            attackNewArmies();
+            break;
+        case StrategyContext::DEFEND_DICE_COUNT:
+            defendNumDice();
+            break;
+        case StrategyContext::FORTIFY_FROM_COUNTRY:
+            fortifyFromCountryIndex();
+            break;
+        case StrategyContext::FORTIFY_TO_COUNTRY:
+            fortifyToCountryIndex();
+            break;
+        case StrategyContext::FORTIFY_ARMY_COUNT:
+            fortifyArmyCount();
+            break;
+        case StrategyContext::REINFORCE_ARMY_COUNT:
+            numArmies();
+            break;
+        case StrategyContext::REINFORCE_CARD_COUNT:
+            place();
             break;
         default: {
         }
@@ -85,3 +161,23 @@ int BenevolentBotStrategy::intInput(StrategyContext context) {
 
     return count;
 }
+
+int BenevolentBotStrategy::attackFromCountryIndex() {};
+
+int BenevolentBotStrategy::attackToCountryIndex() {};
+
+int BenevolentBotStrategy::attackNewArmies() {};
+
+int BenevolentBotStrategy::attackNumDice() {};
+
+int BenevolentBotStrategy::defendNumDice() {};
+
+int BenevolentBotStrategy::fortifyFromCountryIndex() {};
+
+int BenevolentBotStrategy::fortifyToCountryIndex() {};
+
+int BenevolentBotStrategy::fortifyArmyCount() {};
+
+int BenevolentBotStrategy::numArmies() {};
+
+int BenevolentBotStrategy::place() {};
