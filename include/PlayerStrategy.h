dc5466df583230fbd7b7289b5e5fae86cd40f11b
phdumaresq <phdumaresq@gmail.com>
2019-11-11 20:33:31 -0500

    Changed definitions for the functions in PlayerStrategy

--- include/PlayerStrategy.h
+++ include/PlayerStrategy.h
@@ -3,32 +3,47 @@
 #include "Cards.h"
 #include "Dice.h"
 
+enum StrategyContext {
+                      ATTACK,
+                      ATTACK_FROM_COUNTRY,
+                      ATTACK_TO_COUNTRY,
+                      ATTACK_DICE_COUNT,
+                      ATTACK_NEW_ARMY_COUNT,
+                      FORTIFY,
+                      FORTIFY_FROM_COUNTRY,
+                      FORTIFY_TO_COUNTRY,
+                      FORTIFY_ARMY_COUNT,
+                      DEFEND_DICE_COUNT,
+                      REINFORCE,
+                      REINFORCE_CARD_COUNT,
+                      REINFORCE_ARMY_COUNT
+};
+
 #ifndef COMP_345_PROJ_PLAYER_STRATEGY_H
 #define COMP_345_PROJ_PLAYER_STRATEGY_H
 
 class PlayerStrategy {
 public:
-    virtual int attack() = 0;
-    virtual int fortify() = 0;
-    virtual int reinforce() = 0;
+  virtual char yesOrNo(StrategyContext context) = 0;
+  virtual int intInput(StrategyContext context) = 0;
 };
 
 class HumanPlayerStrategy: public PlayerStrategy {
-    int attack();
-    int fortify();
-    int reinforce();
+public:
+  char yesOrNo(StrategyContext _);
+  int intInput(StrategyContext _);
 };
 
 class AggressiveBotStrategy: public PlayerStrategy {
-    int attack();
-    int fortify();
-    int reinforce();
+public:
+  char yesOrNo(StrategyContext context);
+  int intInput(StrategyContext context);
 };
 
 class BenevolentBotStrategy: public PlayerStrategy {
-    int attack();
-    int fortify();
-    int reinforce();
+public:
+  char yesOrNo(StrategyContext context);
+  int intInput(StrategyContext context);
 };
 
 #endif //COMP_345_PROJ_PLAYER_STRATEGY_H
--- src/PlayerStrategy.cpp
+++ src/PlayerStrategy.cpp
@@ -1,13 +1,21 @@
 #include "../include/PlayerStrategy.h"
 
-int HumanPlayerStrategy::attack() { return 0; }
-int HumanPlayerStrategy::fortify() { return 0; }
-int HumanPlayerStrategy::reinforce() { return 0; }
+char HumanPlayerStrategy::yesOrNo(StrategyContext _) {
+  char playerDecision = 0;
+  std::cin >> playerDecision;
 
-int AggressiveBotStrategy::attack() { return 0; }
-int AggressiveBotStrategy::fortify() { return 0; }
-int AggressiveBotStrategy::reinforce() { return 0; }
+  return playerDecision;
+}
 
-int BenevolentBotStrategy::attack() { return 0; }
-int BenevolentBotStrategy::fortify() { return 0; }
-int BenevolentBotStrategy::reinforce() { return 0; }
+int HumanPlayerStrategy::intInput(StrategyContext _) {
+  int count = 0;
+  std::cin >> count;
+
+  return count;
+}
+
+char AggressiveBotStrategy::yesOrNo(StrategyContext context) { return 0; }
+int AggressiveBotStrategy::intInput(StrategyContext context) { return 0; }
+
+char BenevolentBotStrategy::yesOrNo(StrategyContext context) { return 0; }
+int BenevolentBotStrategy::intInput(StrategyContext context) { return 0; }
